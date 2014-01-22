69b0f98f1282a576f9c10e6103d43841e4d19ebb
Alex Vallejo <vallejo.alex@gmail.com>
2014-01-22 15:35:03 -0500

    Complete activity 1

---
+++ budget_calculator.py
@@ -0,0 +1,10 @@
+#Email: amv49@pitt.edu
+#Name: Alex Vallejo
+#ID: 3578411
+#Description: This program is a budget calculator. It will help you calculate
+#             your monthly budget by asking you estimated values for monthly
+#             spending.
+
+print("Author: Alex Vallejo <amv49@pitt.edu>")
+print("\nThis is a budget calculator program. It will help you calculate\
+       \nyour monthly budget")
