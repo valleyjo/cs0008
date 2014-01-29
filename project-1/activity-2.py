c7293c83208f83e6643ab29b5951dde1aa8ca5da
Alex Vallejo <vallejo.alex@gmail.com>
2014-01-29 15:49:45 -0500

    Add activities 2 and 3

---
+++ project-1/activity-2.py
@@ -0,0 +1,33 @@
+hourly_rate = 44
+#Email: amv49@pitt.edu
+#Name: Alex Vallejo
+#ID: 3578411
+#Description: This program is a budget calculator. It will help you calculate
+#             your monthly budget by asking you estimated values for monthly
+#             spending.
+
+overtime_rate = 66
+regular_hours = 160
+overtime_hours = 20
+rent = 550
+electric_bill_percentage = .02
+water_bill_percentage = .02
+sewer_bill_percentage = .01
+gas_bill_percentage = .01
+food = 450
+entertainment = 300
+car = 300
+
+print("Variables Details:\n")
+print("Name: hourly_rate  Value: ", hourly_rate, "Type: ", type(hourly_rate))
+print("Name: overtime_rate  Value: ", overtime_rate, "Type: ", type(overtime_rate))
+print("Name: regular_hours  Value: ", regular_hours, "Type: ", type(regular_hours))
+print("Name: overtime_hours  Value: ", overtime_hours, "Type: ", type(overtime_hours))
+print("Name: rent  Value: ", rent, "Type: ", type(rent))
+print("Name: electric_bill_percentage  Value: ", electric_bill_percentage, "Type: ", type(electric_bill_percentage))
+print("Name: water_bill_percentage  Value: ", water_bill_percentage, "Type: ", type(water_bill_percentage))
+print("Name: sewer_bill_percentage  Value: ", sewer_bill_percentage, "Type: ", type(sewer_bill_percentage))
+print("Name: gas_bill_percentage  Value: ", gas_bill_percentage, "Type: ", type(gas_bill_percentage))
+print("Name: food  Value: ", food, "Type: ", type(food))
+print("Name: entertainment  Value: ", entertainment, "Type: ", type(entertainment))
+print("Name: car  Value: ", car, "Type: ", type(car))
---
+++ project-1/activity-3.py
@@ -0,0 +1,14 @@
+#Email: amv49@pitt.edu
+#Name: Alex Vallejo
+#ID: 3578411
+#Description: This program is a budget calculator. It will help you calculate
+#             your monthly budget by asking you estimated values for monthly
+#             spending.
+
+print("Author: Alex Vallejo <amv49@pitt.edu>")
+print("\nThis is a budget calculator program. It will help you calculate\
+       \nyour monthly budget")
+
+name = input("\nEnter your name: ")
+
+print("\nHello " + name)
