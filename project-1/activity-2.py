ff88488d2e942b0b45460e6dc4698f2339f53e80
Alex Vallejo <vallejo.alex@gmail.com>
2014-01-29 21:09:21 -0500

    add comments to activity-2

--- project-1/activity-2.py
+++ project-1/activity-2.py
@@ -1,4 +1,3 @@
-hourly_rate = 44
 #Email: amv49@pitt.edu
 #Name: Alex Vallejo
 #ID: 3578411
@@ -6,28 +5,40 @@ hourly_rate = 44
 #             your monthly budget by asking you estimated values for monthly
 #             spending.
 
-overtime_rate = 66
-regular_hours = 160
-overtime_hours = 20
-rent = 550
-electric_bill_percentage = .02
-water_bill_percentage = .02
-sewer_bill_percentage = .01
-gas_bill_percentage = .01
-food = 450
-entertainment = 300
-car = 300
 
+hourly_rate = 44                # rate per hour
+overtime_rate = 66              # overtime rate per hour
+regular_hours = 160             # number of reguar hours worked
+overtime_hours = 20             # number of overtime hours worked
+rent = 550                      # cost of rent
+electric_bill_percentage = .02  # cost of electric bill
+water_bill_percentage = .02     # cost of water bill
+sewer_bill_percentage = .01     # cost of sewer bill
+gas_bill_percentage = .01       # cost of gas bill
+food = 450                      # cost of food
+entertainment = 300             # cost of entertainment
+car = 300                       # cost of car
+
+
+# Print the name, value and type of each variable
 print("Variables Details:\n")
 print("Name: hourly_rate  Value: ", hourly_rate, "Type: ", type(hourly_rate))
-print("Name: overtime_rate  Value: ", overtime_rate, "Type: ", type(overtime_rate))
-print("Name: regular_hours  Value: ", regular_hours, "Type: ", type(regular_hours))
-print("Name: overtime_hours  Value: ", overtime_hours, "Type: ", type(overtime_hours))
+print("Name: overtime_rate  Value: ", overtime_rate, "Type: ", \
+      type(overtime_rate))
+print("Name: regular_hours  Value: ", regular_hours, "Type: ", \
+      type(regular_hours))
+print("Name: overtime_hours  Value: ", overtime_hours, "Type: ", \
+      type(overtime_hours))
 print("Name: rent  Value: ", rent, "Type: ", type(rent))
-print("Name: electric_bill_percentage  Value: ", electric_bill_percentage, "Type: ", type(electric_bill_percentage))
-print("Name: water_bill_percentage  Value: ", water_bill_percentage, "Type: ", type(water_bill_percentage))
-print("Name: sewer_bill_percentage  Value: ", sewer_bill_percentage, "Type: ", type(sewer_bill_percentage))
-print("Name: gas_bill_percentage  Value: ", gas_bill_percentage, "Type: ", type(gas_bill_percentage))
+print("Name: electric_bill_percentage  Value: ", electric_bill_percentage,\
+      "Type: ", type(electric_bill_percentage))
+print("Name: water_bill_percentage  Value: ", water_bill_percentage, "Type: ", \
+      type(water_bill_percentage))
+print("Name: sewer_bill_percentage  Value: ", sewer_bill_percentage, "Type: ",\
+      type(sewer_bill_percentage))
+print("Name: gas_bill_percentage  Value: ", gas_bill_percentage, "Type: ", \
+      type(gas_bill_percentage))
 print("Name: food  Value: ", food, "Type: ", type(food))
-print("Name: entertainment  Value: ", entertainment, "Type: ", type(entertainment))
+print("Name: entertainment  Value: ", entertainment, "Type: ", \
+    type(entertainment))
 print("Name: car  Value: ", car, "Type: ", type(car))
